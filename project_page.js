c80ff55a36c05736e90ebfd8518d29e5f4d9737d
Ivan Ivanov <Ivan.Ivanov@softwareag.com>
2019-05-12 14:48:19 +0300

    ADD project page ready; ABWESEND und ZHALEIKA json added

--- project_page.js
+++ project_page.js
@@ -1,15 +1,91 @@
 
 // All the contents of the project pages: 
 let projects = [
+    
+    //ABWESEND
     {
         title: "Abwesend", 
-        id: "abwesend", 
-        cast: ["KATRIN SOMMER", "MÁRTON PETER NAGY", "MIKE SOMMER", "CLAUDIA BALKO", "WERNER ZWOSTA"], 
-        synopsis: "Winter in einer deutschen Großstadt. Klara, 30-jährige alleinerziehende Mutter, kommt mit einem fremden Mann nach Hause. Beide sind betrunken, sie lachen, haben Spaß. Aber es ist noch jemand in der Wohnung: Anton, 9 Jahre alt, entdeckt sie im Bett. Am darauffolgenden Morgen ist Anton weg."
+        id: "abwesend",
+        caroussel_images : [
+            "Abwesend_Still 01.jpg",
+            "Abwesend_Still 02.jpg",
+            "Abwesend_Still 03.jpg",
+            "Abwesend_Still 04.jpg"
+        ], 
+        synopsis: "Winter in einer deutschen Großstadt. Klara, 30-jährige alleinerziehende Mutter, kommt mit einem fremden Mann nach Hause. Beide sind betrunken, sie lachen, haben Spaß. Aber es ist noch jemand in der Wohnung: Anton, 9 Jahre alt, entdeckt sie im Bett. Am darauffolgenden Morgen ist Anton weg.",
+        trailer: "ABWESEND trailer.mp4", 
+        stats_1: [
+            "Kurzfilm, Deutschland 2014, 13 min", 
+            "ARRI Amira, Dolby Digital 5.1"
+        ],
+        stats_2: {
+            Vorführformate: "DCP, Blu-Ray, Quicktime", 
+            Originalsprache: "Deutsch", 
+            Untertitel: "Englisch, Französisch" 
+        },
+        poster : "Abwesend-Poster.jpg",   
+        cast: [
+            "KATRIN SOMMER", 
+            "MÁRTON PETER NAGY", 
+            "MIKE SOMMER", 
+            "CLAUDIA BALKO", 
+            "WERNER ZWOSTA"
+        ], 
+        team: {
+            Buch_und_Regie: "ELIZA PETKOVA", 
+            Producer: "TIM OLIVER SCHULTZ, CÉCILE TOLLU-POLONOWSKI", 
+            Kostümbild: "WARIS KLAMPFER", 
+            Originalton: "HANNES MARGET", 
+            Lichtgestaltung: "DAVID SCHMITT", 
+            Maskenbild: "STELLA VON SENGER", 
+            Regieassistenz_und_Set_AL: "MARCEL SCHROETER", 
+            Kameraassistenz: "NATHANAEL BESSON-VIGO,MARCUS SCHÜTZE", 
+            Licht: "SARA GFRÖRER, GIULIA SCHELHAS", 
+            Aufnahmeleitung: "CARLOTTA CORNEHL", 
+            Schnitt: "ELIZA PETKOVA", 
+            Farbkorrektur: "INGO SCHUMACHER, CONSTANZE SCHMITT", 
+            Tongestaltung: "HANNES MARGET", 
+            Tonmischung: "SILVIO NAUMANN", 
+            Videospiel_Programmierung: "HANNES MARGET", 
+            Herstellungsleitung: "ANDREAS LOUIS", 
+            Produzent: "DEUTSCHE FILM- UND FERNSEHAKADEMIE BERLIN"
+        }, 
+        awards: [
+                "68. Festival de Cannes - Sélection Cinéfondation",   
+                "4th Evolution Mallorca International Film Festival (EMIFF)",   
+                "International Film Festival TOFIFEST, Poland",   
+                "17. Landshuter Kurzfilmfestival, Germany",   
+                "20th Sofia International Film Festival, Bulgaria",   
+                "Ruberoid Festival 2016, Berlin, Germany",   
+                "12th Femina International Women’s Film Festival, Brazil"
+        ]
     }, 
+    
+    //ZHALEIKA
     {
         title: "Zhaleika", 
         id: "zhaleika", 
+        caroussel_images : [
+            "zhaleika_Still 01.jpg",
+            "zhaleika_Still 02.jpg",
+            "zhaleika_Still 03.jpg",
+            "zhaleika_Still 04.jpg",
+            "zhaleika_Still 05.jpg",
+            "zhaleika_Still 06.jpg",
+            "zhaleika_Still 07.jpg"
+        ],
+        synopsis: "Lora, ein 17 jähriges Mädchen wird mit dem Tod ihres Vaters konfrontiert. In dem kleinen bulgarischen Dorf, wo die Zeit stehengeblieben zu sein scheint, erwarten ihre Familie und die Dorfbewohner, dass sie sich ihrer Trauer hingibt. Ab jetzt muss sie Schwarz tragen, darf keine Musik hören und muss diverse Trauerrituale beachten. Dabei will Lora ihre Jugend weiterhin ausleben. Ihr Wunsch aber wird durch den urteilenden Blick der anderen unterbunden, die sie dazu zwingen die Opferrolle eines Waisenkindes zu bedienen.", 
+        trailer: "Zhaleika Trailer.mp4",
+        stats_1: [
+            "Kurzfilm, Deutschland 2014, 13 min", 
+            "ARRI Amira, Dolby Digital 5.1"
+        ],
+        stats_2: {
+            Vorführformate: "DCP, Blu-Ray, Quicktime", 
+            Originalsprache: "Deutsch", 
+            Untertitel: "Englisch, Französisch" 
+        },
+        poster : "Zhaleika Poster.jpg", 
         cast: [
             "ANNA MANOLOVA", 
             "SNEZHINA PETROVA", 
@@ -32,7 +108,7 @@ let projects = [
             Produktionassistenz: "TSVETA ERMENKOVA",
             Kameraassistenz: "MALTE SIEPEN, CRISTINA AMATE GARCIA",
             Set_Runner: "GALIN POPOV, VESSELIN DENEV",
-            Catering: "DIMITRINKA SEMKOVA",
+            Catering: "TEST DIMITRINKA SEMKOVA",
             Schnitt: "HANNES MARGET, ELIZA PETKOVA",
             Tongestaltung: "HANNES MARGET, JOSCHA EICKEL",
             Grading: "CHRISTINE HIAM, CONSTANZE SCHMITT",
@@ -40,8 +116,34 @@ let projects = [
             Geräuschemacher: "PETER ROJGK",
             Herstellungsleitung_DFFB: "ANDREAS LOUIS",
             Produzent: "DEUTSCHE FILM- UND FERNSEHAKADEMIE BERLIN"
-        }, 
-        synopsis: "Lora, ein 17 jähriges Mädchen wird mit dem Tod ihres Vaters konfrontiert. In dem kleinen bulgarischen Dorf, wo die Zeit stehengeblieben zu sein scheint, erwarten ihre Familie und die Dorfbewohner, dass sie sich ihrer Trauer hingibt. Ab jetzt muss sie Schwarz tragen, darf keine Musik hören und muss diverse Trauerrituale beachten. Dabei will Lora ihre Jugend weiterhin ausleben. Ihr Wunsch aber wird durch den urteilenden Blick der anderen unterbunden, die sie dazu zwingen die Opferrolle eines Waisenkindes zu bedienen.", 
+        },
+        awards: [ 
+            "66. Berlinale - Internationale Filmfestspiele Berlin - Generation",
+            "20th International Sofia Film Festival - International Competition",
+            "56th Zlin Film Festival, Czech Republic",
+            "The Golden Linden International Film Festival of New European Cinema, Bulgaria",
+            "31th Festival Internacional de Cine de Valencia CINEMA JOVE, Spain",
+            "2Cinema City International Film Festival Novi Sad, Serbia",
+            "Burgas International Film Festival, Bulgaria",
+            "10. Internationales Fünf Seen Festival, Germany",
+            "Cinematik International Film Festival, Slovakia",
+            "9th Cine East, Luxembourg",
+            "Film Fest Gent, Belgium",
+            "39th Mill Valley Film Festival, California, USA",
+            "Balkanmania, Istanbul Modern, Turkey",
+            "33th Chicago International Children’s Film Festival, USA",
+            "35th Oulu International Children's and Youth Film Festival, Finland",
+            "23th Minsk International Film Festival, Belarus",
+            "Cinedays Skopje, Makedonia",
+            "Zagreb Film Festival, Croatia",
+            "Tbilisi International Film Festival, Georgia",
+            "Küstendorf Film and Music Festival, Serbia",
+            "15th Pune International Film Festival PIFF, India",
+            "Lets Cee Film Festival, Vienna, Austria",
+            "Kinotrip Filmfestival, Ljubljana, Slovenia",
+            "18th Stockholm International Film Festival Junior, Sweden",
+            "Sehsüchte 46th Student Film Festival, Germany" 
+        ], 
     },
 ];
 
@@ -51,13 +153,76 @@ for (let i = 0; i < projects.length; i++) {
     let page = localStorage.getItem("page"); 
     console.log(page);
     if(projects[i].id === page ){
+        
+        //CAPROUSSEL IMAGES 
+        for (let j = 0; j < projects[i].caroussel_images.length; j++) {
+            
+            if(j === 0 ){
+                $(".carousel-inner").append(`
+                    <div class="carousel-item active">
+                        <img class="d-block w-100" src="Image/${projects[i].caroussel_images[j]}" alt="Slide_${j}">
+                    </div>
+                `); 
+            }else{
+                $(".carousel-inner").append(`
+                    <div class="carousel-item">
+                        <img class="d-block w-100" src="Image/${projects[i].caroussel_images[j]}" alt="Slide_${j}">
+                    </div>
+                `);
+            }
+        }
+        
+        //SYNOPSIS
         let synopsisEl = document.querySelector("#synopsis");
         synopsisEl.innerHTML= projects[i].synopsis; 
-        let castEl = document.querySelector("#cast_id");
+        
+        //CAST
         for (let j = 0; j < projects[i].cast.length; j++) {
             $("#cast_id").append(`
             <p>${projects[i].cast[j]}</p>
             `); 
         }
-    };
-};
+
+        //TEAM
+        const entries = Object.entries(projects[i].team);
+        for(const [role, person] of entries){
+            const newRole = role.replace(/_/g, " "); 
+            $("#team_id").append(`
+                <p>${newRole} : ${person}</p>
+            `);     
+        }; 
+
+        //STATS_1
+        for (let j = 0; j < projects[i].stats_1.length; j++) {
+            $("#stats_1_id").append(`
+            <p>${projects[i].stats_1[j]}</p>
+            `); 
+        }
+
+        //STATS_2
+        const entries2 = Object.entries(projects[i].stats_2);
+        for(const [key, value] of entries2){
+            // const newRole = role.replace(/_/g, " "); 
+            $("#stats_2_id").append(`
+            <p>${key} : ${value}</p>
+            `);
+        };
+
+        //AWARDS 
+        for (let j = 0; j < projects[i].awards.length; j++) {
+            $("#awards_id").append(`
+                <p><img src="Image/video-camera.png">${projects[i].awards[j]}</p>   
+            `); 
+        }
+
+         //TITLE 
+         $('#film_title').html(`${projects[i].title.toUpperCase()}`); 
+
+        //POSTER 
+        $("#poster_id").attr("src",`poster/${projects[i].poster}`);
+
+        //TRAILER
+        $("#trailer_id").attr("src",`trailer/${projects[i].trailer}`);
+
+    };  
+}; 
