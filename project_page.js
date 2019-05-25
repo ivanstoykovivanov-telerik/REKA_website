e5bf43aab7e669f4ebc89a6cdce034a5a9af7d15
Ivan Ivanov <Ivan.Ivanov@softwareag.com>
2019-05-25 23:07:58 +0300

    'Willkommen zu Hause', 'Die Anderen', 'Mein Sohn' added to projects

--- project_page.js
+++ project_page.js
@@ -60,69 +60,248 @@ let projects = [
                 "12th Femina International Women’s Film Festival, Brazil"
         ]
     }, 
+    
+    {
+        title: "DIE ANDEREN",
+        id: "dieanderen",
+        caroussel_images : [
+            "DIE ANDEREN_Still01_1.jpg",
+            "DIE ANDEREN_Still02_1.png",
+            "DIE ANDEREN_Still03_1.png"
+        ],
+        synopsis: "Jonas verfügt über eine besondere Fähigkeit, die er aber vor den anderen verstecken muss, damit er weiterhin in der Gesellschaft fortbestehen kann, die keine Abweichungen von der Norm duldet. Er versucht Erwartungen zu entsprechen und entfernt sich somit immer mehr von sich selbst. Am Ende steht er vor der Entscheidung - Sich selbst treu zu bleiben oder sich für die Anerkennung anderer aufzugeben.",
+        trailer: "DieAnderen_Trailer_DE-en_20190330_kleiner.mp4",
+        stats_1: [
+            "Kurzfilm, Deutschland 2018, 11:13 min",
+            "ARRI Amira, Dolby Digital 5.1"
+        ],
+        stats_2: {
+            Vorführformate: "DCP, Blu-Ray, Quicktime",
+            Originalsprache: "Deutsch",
+            Untertitel: "Englisch"
+        },
+        poster : "Die Anderen Poster.jpg",
+        cast: [
+            "MÁRTON PETER NAGY"
+        ],
+        team: {
+            Buch_und_Regie: "ELIZA PETKOVA",
+            Bildgestaltung: "KONSTANTIN KANN",
+            Kostümbild: "WARIS KLAMPFER",
+            Kameraassistenz: "SABINE BALLNAT",
+            Schnitt: "ELIZA PETKOVA",
+            Tongestaltung: "HANNES MARGET",
+            Farbkorrektur: "INGO SCHUMACHER",
+            Tonmischung: "SILVIO NAUMANN",
+            Herstellungsleitung_DFFB: "ANDREAS LOUIS",
+            Produzent: "DEUTSCHE FILM- UND FERNSEHAKADEMIE BERLIN, REKA PICTURES"
+        },
+        awards: [
+            "48th Lubuskie Filmsommer, Polen",
+            "52nd Hof International Film Festival"
+        ]
+    }, 
 
     {
         title: "EIN FISCH, DER AUF DEM RÜCKEN SCHWIMMT",
         id: "einfisch",
         caroussel_images : [
-        "einfisch.jpg"
+            "einfisch.jpg"
         ],
         synopsis: "Andrea, eine Frau ohne Vergangenheit – verspielt, unmittelbar, leicht, energetisch, ehrlich, unberechenbar. Sie ist ein Kind. Philipp und Martin sind Vater und Sohn – beide lieben sie. Andreas Anwesenheit soll die Lücke der verstorbenen Hanna füllen. Es folgt ein Sommer der erfüllten Sehnsüchte. Doch die innere Leere beginnt nach Trost, nach Bindung und nicht zuletzt nach Sicherheit zu suchen. Sie will besitzen. Die Liebe wird durch Angst verdrängt und die Gegenwart verschwindet mit der Frage nach dem morgigen Tag. Drei Personen. Ein Haus. Sie leben und handeln jenseits der Gesellschaftsnormen und scheitern dann doch an ihren viel zu menschlichen Bedürfnissen. Am Ende bleiben die Leere und die Frage nach der Schuld, die nicht verortet werden kann.",
         trailer: "",
         stats_1: [
-        "Langspielfilm, Deutschland 2019, 105 min",
-        "ARRI Amira, Dolby Surround 5.1"
+            "Langspielfilm, Deutschland 2019, 105 min",
+            "ARRI Amira, Dolby Surround 5.1"
         ],
         stats_2: {
-        Vorführformate: "DCP, Blu-Ray, Quicktime",
-        Originalsprache: "Deutsch",
-        Untertitel: "Englisch"
+            Vorführformate: "DCP, Blu-Ray, Quicktime",
+            Originalsprache: "Deutsch",
+            Untertitel: "Englisch"
         },
         poster : "",
         cast: [
-        "NINA SCHWABE",
-        "THEO TREBS",
-        "HENNING KOBER",
-        "ANNA MANOLOVA",
-        "MÁRTON PETER NAGY",
-        "LEON ULLRICH"
+            "NINA SCHWABE",
+            "THEO TREBS",
+            "HENNING KOBER",
+            "ANNA MANOLOVA",
+            "MÁRTON PETER NAGY",
+            "LEON ULLRICH"
         ],
         team: {
-        Buch_und_Regie: "ELIZA PETKOVA",
-        Bildgestaltung: "CONSTANZE SCHMITT",
-        Producer: "KONSTANTIN KANN",
-        Produktionsassistenz: "NEELE SCHRÖDER, JULITA WITT, MARIE NORDMANN",
-        Kostüm_und_Maskenbild: "NURIA HEYCK, ANNA PHILIPPA MÜLLER",
-        Szenenbild: "MIREN OLLER",
-        Innenrequisite: "ANN-CHRISTIN HANSEN, TIZIANA SCHINDLER",
-        Szenenbildassistenz: "HAUKE MAGUHN, JOHN MCINTOSH, EHSAN MORSHED SEFAT, PAULINA CONSTANZA RADIC LOPEZ, JO-MORITZ KRAH, SVEN PHILIPP POHL",
-        Steadicam: "KONSTANTIN KANN",
-        Oberbeleuchter: "DAVID SCHMITT",
-        Beleuchter: "ANNA ILIN, SINA ESLAMI, LYDIA RICHTER, MALTE SIEPEN, BEN BERNHARD, NINA REICHMANN, PAUL NÄTHER",
-        Standfotograf: "LOK KINWAH",
-        Originalton: "HANNES MARGET, EMIL MORGENSTERN",
-        Regieassistenz: "WILLY KRISTEN",
-        Kameraassistenz: "SIRINTON KAOMANIT, MALTE SIEPEN, CHRISTINA AMATE GARCIA, NORWIN HATSCHBACH, ELIAS FRITZ, DANIEL MERGET, JENS HALLMANN, JULIAN SCHMITT, KATHARINA HAUKE, KONSTANTIN MINNICH, PAUL ROHLFS",
-        Tonassistenz: "VAZEH MUSTAFA", 
-        Setassistenz: "PAVEL BOZHILOV, JANEK NEAL LOUIS",
-        Garderobiere: "CHARLOTTE SCHWÄRMER",
-        Schnitt: "ELIZA PETKOVA, HANNES MARGET",
-        Sound_Design: "HANNES MARGET", 
-        Motion_Graphics: "GUNAR LAUBE, DIRK HEYMANN",
-        Tonmischung: "ALEXANDRE LESER",
-        Geräuschemacher: "PETER ROIGK",            
-        Farbkorrektur: "INGO SCHUMACHER",
-        Herstellungsleitung_DFFB: "ANDREAS LOUIS",
-        Produzent: "DEUTSCHE FILM- UND FERNSEHAKADEMIE BERLIN",
-        Ko_Produzenten: "DAS KIND MIT DER GOLGENEN JACKE FILMPRODUKTION, REKA PICTURES, WILD GRASS FILMS",
+            Buch_und_Regie: "ELIZA PETKOVA",
+            Bildgestaltung: "CONSTANZE SCHMITT",
+            Producer: "KONSTANTIN KANN",
+            Produktionsassistenz: "NEELE SCHRÖDER, JULITA WITT, MARIE NORDMANN",
+            Kostüm_und_Maskenbild: "NURIA HEYCK, ANNA PHILIPPA MÜLLER",
+            Szenenbild: "MIREN OLLER",
+            Innenrequisite: "ANN-CHRISTIN HANSEN, TIZIANA SCHINDLER",
+            Szenenbildassistenz: "HAUKE MAGUHN, JOHN MCINTOSH, EHSAN MORSHED SEFAT, PAULINA CONSTANZA RADIC LOPEZ, JO-MORITZ KRAH, SVEN PHILIPP POHL",
+            Steadicam: "KONSTANTIN KANN",
+            Oberbeleuchter: "DAVID SCHMITT",
+            Beleuchter: "ANNA ILIN, SINA ESLAMI, LYDIA RICHTER, MALTE SIEPEN, BEN BERNHARD, NINA REICHMANN, PAUL NÄTHER",
+            Standfotograf: "LOK KINWAH",
+            Originalton: "HANNES MARGET, EMIL MORGENSTERN",
+            Regieassistenz: "WILLY KRISTEN",
+            Kameraassistenz: "SIRINTON KAOMANIT, MALTE SIEPEN, CHRISTINA AMATE GARCIA, NORWIN HATSCHBACH, ELIAS FRITZ, DANIEL MERGET, JENS HALLMANN, JULIAN SCHMITT, KATHARINA HAUKE, KONSTANTIN MINNICH, PAUL ROHLFS",
+            Tonassistenz: "VAZEH MUSTAFA", 
+            Setassistenz: "PAVEL BOZHILOV, JANEK NEAL LOUIS",
+            Garderobiere: "CHARLOTTE SCHWÄRMER",
+            Schnitt: "ELIZA PETKOVA, HANNES MARGET",
+            Sound_Design: "HANNES MARGET", 
+            Motion_Graphics: "GUNAR LAUBE, DIRK HEYMANN",
+            Tonmischung: "ALEXANDRE LESER",
+            Geräuschemacher: "PETER ROIGK",            
+            Farbkorrektur: "INGO SCHUMACHER",
+            Herstellungsleitung_DFFB: "ANDREAS LOUIS",
+            Produzent: "DEUTSCHE FILM- UND FERNSEHAKADEMIE BERLIN",
+            Ko_Produzenten: "DAS KIND MIT DER GOLGENEN JACKE FILMPRODUKTION, REKA PICTURES, WILD GRASS FILMS",
         },
         awards: [
-        "48th Lubuskie Filmsommer, Polen",
-        "52nd Hof International Film Festival"
+            "48th Lubuskie Filmsommer, Polen",
+            "52nd Hof International Film Festival"
         ]
     },
-    
-    //ZHALEIKA
+    {
+        title: "Zur rechten Zeit",
+        id: "zurrechtenzeit",
+        caroussel_images : [
+            "zurrechtenzeit_Still 1.jpg",
+            "zurrechtenzeit_Still 2.jpg",
+            "zurrechtenzeit_Still 3.jpg",
+            "zurrechtenzeit_Still 4.jpg",
+            "zurrechtenzeit_Still 5.jpg",
+        ],
+        synopsis: "Adriane Gertz ist Philosophielehrerin in der Oberstufe. Ihr Ideal sozial unabhängig zu sein und ihr Bestreben nach Selbstgenügsamkeit werden durch die Liebesbeziehung zu ihrem Schüler Dominik erschüttert. Die Unmöglichkeit ihren Werten gerecht zu werden bringt sie zur Selbstverachtung. Ihr Alltag wird zunehmend von Machtspielen und Verlustängsten bestimmt. Adriane muss schmerzhaft erfahren, dass sie sich dem Menschlichen nicht entziehen kann.Die Verneinung der Körperlichkeit durch Waschzwänge, die Vermeidung von Begegnungen durch soziale Isolation, zeigen das Bild einer Frau, die die Kälte und Einsamkeit einer Gesellschaft verkörpert, in der die Angst vorm Scheitern die Lust zum Leben erstickt.",
+        stats_1: [
+            "Kurzfilm, Deutschland 2014, 22 min",
+            "RED One, Dolby Digital 5.1"
+        ],
+        stats_2: {
+            Vorführformate: "DCP, Blu-Ray, Quicktime",
+            Originalsprache: "Deutsch",
+            Untertitel: "Englisch"
+        },
+        poster : "", 
+        cast: [
+            "KIRSTEN ALBSMEIER",
+            "MÁRTON PETER NAGY",
+            "KATRIN SOMMER",
+            "MICHAEL DANISCH",
+            "FRANK KALLINOWSKI",
+            "PAULA MATHEIS",
+            "JAKOB D'APRILE",
+        ],
+        team: {
+            Buch_und_Regie: "ELIZA PETKOVA",
+            Bildgestaltung : "JAKOB KRÜGER",
+            Produktion: "CLEMENS KÖSTLIN",
+            Kostümbild: "WARIS KLAMPFER",
+            Szenenbild: "ANNA-SOPHIA EPPSTEIN, HENDRIKE ROERS",
+            Originalton : "HANNES MARGET",
+            Lichtgestaltung: "CHRISIAN TRIELOFF, CONSTANZE SCHMITT",
+            Maskenbild:  "EMOKE SAMU",
+            Regieassistenz_und_Set_AL: "MARCEL SCHROETER",
+            Produktionassistenz: "HENNING WAGNER",
+            Kameraassistenz: "NATHANAEL BESSON-VIGO",
+            Zweite_Kameraassistent: "JONAS FRIEDRICH",
+            Licht: "GIULIA SCHELHAAS, MORITZ BAUER, SARA GFÖRER",
+            Tonassistenz: "PETER LORENZ",
+            Schnitt: "HANNES MARGET, ELIZA PETKOVA",
+            Grading: "DANIEL STÜBNER",
+            Tongestaltung: "HANNES MARGET",
+            Tonmischung: "SILVIO NAUMANN",
+            Herstellungsleitung_DFFB: "MYRIAM EICHLER",
+            Produzent: "DEUTSCHE FILM- UND FERNSEHAKADEMIE BERLIN"
+        },
+        awards: [
+            "48. Internationale Hofer Filmtage",
+            "Shortcutz, Berlin",
+        ]
+    },    
+    {
+        title: "Mein Sohn",
+        id: "meinsohn",
+        caroussel_images : [
+            "meinsohn_Still 1.jpg",
+            "meinsohn_Still 2.jpg",
+            "meinsohn_Still 3.jpg",
+        ],
+        synopsis: "Leonard von Thiel hat Jura studiert und sollte die Kanzlei seines Vaters übernehmen. Stattdessen verkauft er tropische Früchte in einem Bonbon-Outfit und versucht damit andere Menschen glücklich zu machen. Ein Doc-Fiction Film, der unveränderte Realität und inszenierte Wirklichkeiten vermischt und die Frage nach dem „Richtig“ und „Falsch“ erst gar nicht zu stellen vermag.",
+        stats_1: [
+            "Docfiction, Kurzfilm, Deutschland 2013, 17:47 min, HD",
+        ],
+        stats_2: {
+            Vorführformate: "Blu-Ray, Quicktime",
+            Originalsprache: "Deutsch",
+            Untertitel: "Englisch"
+        },
+        poster : "", 
+        cast: [
+            "KIM SCHIMMLER",
+            "CLAUDA BALKO",
+            "MICHAEL DANISCH",
+        ],
+        team: {
+            Buch_und_Regie: "ELIZA PETKOVA",
+            Bildgestaltung : "JAKOB KRÜGER",
+            Tongestaltung: "HANNES MARGET",
+            Produktion: "CINEMANDA & KRÜGER",
+        },
+        awards: [
+            "INTERFILM 29th International Shortfilmfestival, Berlin",
+            "European Media Art Festival (EMAF), Osnabrück",
+            "26. Exground Filmfest, Wiesbaden",
+            "Bangkok International Student Film Festival, Thailand",
+            "Buldoc Festival Berlin",
+            "Festival Of Nations, Österreich",
+            "Shortcutz, Berlin",
+            "Shorts Attack",
+            "Berlin Art Film Festival",
+        ]
+    },
+    {
+        title: "Willkommen zuhause",
+        id: "willkommenzuhause",
+        caroussel_images : [
+            "willkommenzuhause1.jpg",
+            "willkommenzuhause2.jpg",
+            "willkommenzuhause3.jpg",
+        ],
+        synopsis: " Am 14. April 2010 wurde das Rückübernahmeabkommen zwischen Deutschland und Kosovo unterzeichnet. Seitdem finden zunehmend Abschiebungen von aus dem Kosovo stammenden Roma statt. In den Flugzeugen sitzen Menschen, die meistens seit über zehn Jahren in Deutschland leben und nun im Kosovo ihre Heimat finden sollen. Wie gelingt es jemandem, der in Deutschland geboren und aufgewachsen ist, sich in einem fremden Land zu Hause zu fühlen, ohne dass er die Sprache beherrscht, ohne die Möglichkeit, die Straße in Ruhe und Sicherheit zu betreten? Wie viele Träume und Hoffnungen bleiben da noch übrig? Und wie fühlt man sich, wenn 40 Polizisten mitten in der Nacht an der Tür klingeln mit der Ankündigung: „Ihr habt 30 Minuten Zeit, eure Sachen zu packen.“? Der Film ermöglicht einen Einblick in das Leben abgeschobener Roma im Kosovo und solchen, die in Deutschland von der Abschiebung bedroht sind. Keines der abgeschobenen Kinder besucht die Schule; meistens übernachten alle Familienmitglieder in einem Raum; medizinische Versorgung ist den Roma nur schwer zugänglich; die wenigsten besitzen ein Bad, und sogar fließendes Wasser in der Toilette zählt zum Luxus. Hier wie dort können die Roma nachts nicht ruhig schlafen. In Deutschland, weil sie jede Sekunde mit der Abschiebung rechnen, im Kosovo vor lauter Heimweh nach Deutschland - und aus Angst vor rassistischen Übergriffen..",
+        stats_1: [
+        " Dokumentarfilm, Deutschland, Kosovo, 2011, 70 min, HD",
+        ],
+        stats_2: {
+            Vorführformat: "Quicktime",
+            Originalsprache: "Deutsch",
+            Untertitel: "Englisch"
+        },
+        poster : "", 
+        cast: [], 
+        team: {
+            Buch_und_Regie: "ELIZA PETKOVA",
+            Kamera: "HANNES MARGET",
+            Originalton: "JOHANNES KUHN",
+            Producer: "JAN DERKSEN",
+            Recherche: "EVA WEBER",
+            Schnitt: "HANNES MARGET, ELIZA PETKOVA",
+            Tongestaltung: "SILVAN OSCHMANN",
+            Tonmischung: "HANNES MARGET",
+            Farbkorrektur: "MAURICE BRAUN",
+            Untertitel: "JOHN NEILAN",
+            Produktion: "Cinemanda, Chun & Derksen",
+        },
+        awards: [
+            "Globale Mittelhessen 2013",
+            "Videodrom Salzburg 2013",
+            "Globale Berlin 2011",
+            "39th Festival of Nations",
+            "Transeuropa Filmfestival",
+            "Balkan Cinema Filmfestival Bremen",
+            "Rolling Film Festival Kosovo",
+        ]
+    }, 
     {
         title: "Zhaleika", 
         id: "zhaleika", 
