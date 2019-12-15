5b5858e121b5d01c8d6b293ad39c0ec112d9c3a0
Ivan Ivanov <Ivan.Ivanov@softwareag.com>
2019-12-15 22:46:32 +0200

    Add news to Starting page ; Translate Starting pagel; correct bio info page for Eliza and Konstantin

---
+++ startseite.js
@@ -0,0 +1,18 @@
+const news = [
+         "SOUNDS OF NATURE ist fertig gedreht und wir freuen uns auf die Fertigstellung.", 
+         "DIE ANDEREN feiert Premiere auf den 52. Internationalen Hofer Filmtagen.",
+         "EIN FISCH, DER AUF DEM RÜCKEN SCHWIMMT wurde abgedreht. Jetzt geht es in die Postproduktion.",
+         "EIN FISCH, DER AUF DEM RÜCKEN SCHWIMMT läuft auf der 70. Berlinale in der Perspektive Deutsches Kino – wir freuen uns sehr!"
+];
+
+for (let i = news.length-1; i >= 0; i--) {
+    $(".news-container").append(
+        `<div class="row">   
+            <div class="col-2"><img src="Image/Vogel_Symbol_News.png" alt="" class="bird-news"></div>
+            <div class="col-9 news" data-translate="_news_${i + 1}">${news[i]}</div>  
+        </div>   
+        `
+    );
+};
+
+
